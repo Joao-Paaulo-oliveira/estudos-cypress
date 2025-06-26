dbf2d1778c0136780e49aa4b1820980043457306
João Paulo <seuemail@exemplo.com>
2025-06-26 20:31:44 -0300

    adicionando interações

--- cypress/e2e/interagindo.cy.js
+++ cypress/e2e/interagindo.cy.js
@@ -1,12 +1,92 @@
-describe('Preenchendo campos com o cypress', () => {
-  it('Preenchendo', () => {
+
+// Preenchendo Campos
+
+      it('Preenchendo', () => {
    
     cy.visit('/')
     cy.get('.form-control')
       .type('joao.ppaulo92@qa.com')
     
 
+  })
 
+// Clicando em um botão
 
+describe('Clicando ', () => {
+      it('Click', () => {
+   
+    cy.visit('/')
+    cy.get('.fa-user')
+      .click()
+    
   })
 })
+
+// Duplo click em botão
+
+describe('Duplo click', () => {
+      it('Duplo click', () => {
+   
+    cy.visit('/')
+    cy.get('.fa-user')
+      .dblclick()
+    
+  })
+})
+
+// Clicando com o botão direito 
+
+describe('Click botão direito', () => {
+      it('Click botão direito', () => {
+   
+    cy.visit('/')
+    cy.get('.fa-user')
+      .rightclick()
+    
+  })
+})
+
+// Clicando por cordenada 
+
+describe('click por cordenada', () => {
+      it('click por cordenada', () => {
+   
+    cy.visit('/')
+    cy.get('.fa-user')
+      .click(100, 100, {force: true})
+    
+  })
+})
+
+ // Apertando enter
+
+
+describe('Apertando enter', () => {
+   it('Apertando enter', () => {
+   
+    cy.visit('/')
+    cy.get('.form-control')
+      .type('joao.ppaulo92@qa.com{enter}')
+    
+
+  })
+
+})
+
+// Clicando com Select 
+
+describe('Select', () => {
+   it('Apertando Select', () => {
+   
+    cy.visit('/')
+    cy.get('.footer_one_widget')
+      .contains('Checkout View Two')
+      .click()
+   
+    
+    cy.get('#country')
+      .select('Algeria')
+  })
+
+})
+
