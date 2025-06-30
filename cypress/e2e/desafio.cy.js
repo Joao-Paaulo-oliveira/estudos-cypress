070a72c0828a722e08c679fad1b5c72a3454e6fb
João Paulo <seuemail@exemplo.com>
2025-06-30 20:12:49 -0300

    Validar campo e-mail vazio

--- cypress/e2e/desafio.cy.js
+++ cypress/e2e/desafio.cy.js
@@ -14,10 +14,38 @@ describe('Cadastro de Usuario', () => {
         .get('#user')  // Verifica se o campo de usuário está visível
         .should('be.visible') // Verifica se o campo de usuário está visível
 
-     cy.get('#btnRegister') // Verifica se o botão de registro está visível
+     cy.get('#btnRegister') // Verifica se o botão de cadastro está visível
          .click() // Clica no botão de registro
 
      cy.get('.errorLabel') // Verifica se o rótulo de erro está visível
          .should('have.text' , 'O campo nome deve ser prenchido') // Verifica se o texto do rótulo de erro é o esperado
   })
+
+it.only('Validar campo e-mail vazio', () =>{
+     
+    cy.visit('/') // Acessa a página inicial
+    
+    cy.get('.header-logo') // Verifica se o logo está visível
+        .get('.fa-lock')// Verifica se o ícone de bloqueio está visível
+        .click()// Clica no ícone de bloqueio para acessar o login
+ 
+        .get('#user')  // Verifica se o campo de usuário está visível
+        .should('be.visible') // Verifica se o campo de usuário está visível
+        .type('João Paulo') // Digita o nome de usuário no campo
+
+    cy.get('#email') // Verifica se o campo de e-mail está visível
+          .should('be.visible') // Verifica se o campo de e-mail está visível
+          .type(' ')// Limpa o campo de e-mail para simular um campo vazio
+    
+    cy.get('#btnRegister') // Verifica se o botão de cadastro está visível
+         .click() // Clica no botão de registro
+
+    cy.get('#errorMessageFirstName') // Verifica se o rótulo de erro do campo de e-mail está visível
+          .should('have.text', 'O campo e-mail deve ser prenchido corretamente') // Verifica se o texto do rótulo de erro é o esperado
+
+
 })
+
+
+
+})   
