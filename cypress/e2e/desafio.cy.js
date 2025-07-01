d7b002586d1ed3957e3b2ac9a884f094485a511f
João Paulo <seuemail@exemplo.com>
2025-06-30 21:28:32 -0300

    Cadastro realizado com sucesso

--- cypress/e2e/desafio.cy.js
+++ cypress/e2e/desafio.cy.js
@@ -82,10 +82,63 @@ describe('Cadastro de Usuario', () => {
     cy.get('#btnRegister') // Verifica se o botão de cadastro está visível
       .click(); // Clica no botão de registro
 
-    cy.get('.errorLabel').should(  // Verifica se o rótulo de erro está visível
-      'have.text', // Verifica se o texto do rótulo de erro é o esperado
-      'O campo senha deve ter pelo menos 6 dígitos' // Verifica se o texto do rótulo de erro é o esperado
-    );
-  });
+  cy.get('.errorLabel').should(
+    'have.text',
+    'O campo senha deve ter pelo menos 6 dígitos'
+  );
 });
 
+it('Campo senha invalido', () => {
+  cy.visit('/');
+
+    cy.get('.header-logo') // Verifica se o logo está visível
+      .get('.fa-lock') // Verifica se o ícone de bloqueio está visível
+      .click() // Clica no ícone de bloqueio para acessar o login
+
+      .get('#user') // Verifica se o campo de usuário está visível
+      .should('be.visible') // Verifica se o campo de usuário está visível
+      .type('João Paulo'); // Digita o nome de usuário no campo
+
+    cy.get('#email') // Verifica se o campo de e-mail está visível
+      .should('be.visible') // Verifica se o campo de e-mail está visível
+      .type('joao.ppaulo@qa.com.br'); // Limpa o campo de e-mail para simular um campo vazio
+
+    cy.get('#password').should('be.visible').type('1234'); // digita uma senha inválida (menos de 6 dígitos)
+
+    cy.get('#btnRegister') // Verifica se o botão de cadastro está visível
+      .click(); // Clica no botão de registro
+  cy.get('.errorLabel').should(
+    'have.text',
+    'O campo senha deve ter pelo menos 6 dígitos'
+  );
+});
+
+it.only('Cadastro realizado com sucesso', () => {
+  cy.visit('/');
+    cy.visit('/');
+
+    cy.get('.header-logo') // Verifica se o logo está visível
+      .get('.fa-lock') // Verifica se o ícone de bloqueio está visível
+      .click() // Clica no ícone de bloqueio para acessar o login
+
+      .get('#user') // Verifica se o campo de usuário está visível
+      .should('be.visible') // Verifica se o campo de usuário está visível
+      .type('João Paulo'); // Digita o nome de usuário no campo
+
+    cy.get('#email') // Verifica se o campo de e-mail está visível
+      .should('be.visible') // Verifica se o campo de e-mail está visível
+      .type('joao.ppaulo@qa.com.br'); // Limpa o campo de e-mail para simular um campo vazio
+
+    cy.get('#password')
+    .should('be.visible')
+    .type('123456'); // digita uma senha válida (6 dígitos)
+
+    cy.get('#btnRegister') // Verifica se o botão de cadastro está visível
+      .click(); // Clica no botão de registro
+    cy.get('.swal2-title').should(
+      'have.text',
+      'Cadastro realizado!'
+    );
+  });
+
+});
